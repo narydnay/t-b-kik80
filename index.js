1efa81ea4d250e0e2fccda32bdad4a218141d770
Alekseyua <alekseyuadnepr@gmail.com>
2024-03-07 23:42:25 +0200

    add bot v8

--- index.js
+++ index.js
@@ -5,19 +5,16 @@ const bodyParser = require('body-parser');
 const routers = require("./src/routers/admin..router");
 
 const app = express();
-
 app.use(bodyParser.json())
 app.use(bot.webhookCallback(config.getWebHookpath('shot')));
 bot.telegram.setWebhook(config.getWebHookpath('full'));
 app.use(routers)
-
-app.get("/", (req, res) => {
-  res.send("Express on Vercel--- add telegraf v7");
-});
-
 app.listen(5000, () => {
   console.log("Running on port 5000.");
 });
 
+
 // Export the Express API
+
+
 module.exports = app;
