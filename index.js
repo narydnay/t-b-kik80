e5d0f2999995f49c2a801ca9c64202816ab68f92
Alekseyua <alekseyuadnepr@gmail.com>
2024-03-07 21:00:58 +0200

    add telegraf

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
 const express = require("express");
-
+const { Telegraf } = require("telegraf");
+const TOKEN = '6884974307:AAEN0vj63vJ0ntxRoVSiqSnupPg3S2h7ymc';
+const WH_PATH = '/bot' + TOKEN;
+const bot = new  Telegraf(TOKEN);
 const app = express();
+
+bot.telegram.setWebhook('https://beige-worlds-matter.loca.lt' + WH_PATH)
+app.use(bot.webhookCallback(WH_PATH))
+
+bot.on('text', ctx => {
+  ctx.reply('hi bro we work good')
+})
 
 app.get("/", (req, res) => {
   res.send("Express on Vercel-------------------------------------------------");
