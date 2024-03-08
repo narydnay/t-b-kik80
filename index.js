841abb4211883fd81494983811bac42045329ffd
Alekseyua <alekseyuadnepr@gmail.com>
2024-03-08 08:50:37 +0200

    add connect  handler

--- index.js
+++ index.js
@@ -3,15 +3,19 @@ const bot = require('./src/telegram/index');
 const config = require('./src/config/config');
 const bodyParser = require('body-parser');
 const routers = require("./src/routers/admin..router");
-const { noFound } = require("./src/middlewares/noFound");
+const { noFound, errorHandler } = require("./src/middlewares/noFound");
 
 const app = express();
 app.use(bodyParser.json())
+
 app.use(bot.webhookCallback(config.getWebHookpath('shot')));
 bot.telegram.setWebhook(config.getWebHookpath('full'));
-app.use(routers)
 
-app.use(noFound)
+app.use(routers);
+
+app.use(noFound);
+app.use(errorHandler);
+
 app.listen(5001, () => {
   console.log("Running on port 5001.");
 });
