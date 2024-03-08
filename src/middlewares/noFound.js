a1c952a08941fefb4e319d790553fc32afd31079
Alekseyua <alekseyuadnepr@gmail.com>
2024-03-08 08:49:08 +0200

    add error handler

--- src/middlewares/noFound.js
+++ src/middlewares/noFound.js
@@ -1,11 +1,17 @@
+
 function noFound(req, res, next){
   const error = new Error(`Ulr not found ${req.originalUrl}`);
   res.status(404);
   return next(error);
 }
 
+function errorHandler(err, req,res){
+  const statusCode = req.statusCode === 200? 500 : res.statusCode;
+  res.status(statusCode);
+  return res.json({success: false, message: err.message});
+}
 
 module.exports = {
   noFound,
-  
+  errorHandler
 }
