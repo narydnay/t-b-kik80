5157f0bf81a87d4c63436bc59ef28e6e897c7c45
Alekseyua <alekseyuadnepr@gmail.com>
2024-03-10 13:38:29 +0200

    add axios v5

--- src/telegram/index.js
+++ src/telegram/index.js
@@ -12,7 +12,7 @@ bot.on('text', async ctx => {
   const { message } = ctx;
   if(message.text === 'список' ){
     try{
-      const listPrisoner = await axios.get('http://localhost:5001/api/get-all-prisoners')
+      const listPrisoner = await axios.get('https://t-bot-kik.vercel.app/api/get-all-prisoners')
       return ctx.reply('hi bro we work good, what are doing? ...' + JSON.stringify(listPrisoner.data, null, 4))
     }catch(err){
       return ctx.reply('ERROR? ...\n' + JSON.stringify(err, null, 4))
