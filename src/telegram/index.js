ba9018d2b3f75a66c6703857fa858a7029a7e920
Alekseyua <alekseyuadnepr@gmail.com>
2024-03-10 12:54:00 +0200

    add telegram obtain data  v1

--- src/telegram/index.js
+++ src/telegram/index.js
@@ -1,18 +1,18 @@
 const { Telegraf } = require("telegraf");
 const config = require("../config/config");
-// const { queryDataBase } = require("../models/models");
+const { queryDataBase } = require("../models/models");
 const TOKEN = config.get('token-bot');
 
 const bot = new Telegraf(TOKEN);
-// const dbFirebase = new queryDataBase();
+const dbFirebase = new queryDataBase();
 
 bot.on('text', async ctx => {
   const { message } = ctx;
-  // if(message.text === 'список' ){
-  //   const listPrisoner = await dbFirebase.getDataFromDb({nameField: 'name', qOperant: '!=', value:false});            
-  //   ctx.reply('hi bro we work good, what are doing? ...' + JSON.stringify(listPrisoner.splice(0,3), null, 4))
+  if(message.text === 'список' ){
+    const listPrisoner = await dbFirebase.getDataFromDb({nameField: 'name', qOperant: '!=', value:false});            
+    ctx.reply('hi bro we work good, what are doing? ...' + JSON.stringify(listPrisoner.splice(0,3), null, 4))
     
-  // }
+  }
 
   ctx.reply('hi bro we work good, what are doing? ...' + JSON.stringify(message, null, 4))
 })
