ee84e361c37f6dec8c8c112bf21716482abf5303
Alekseyua <alekseyuadnepr@gmail.com>
2024-03-10 14:54:50 +0200

    add test  v3

--- src/controllers/test.controller.js
+++ src/controllers/test.controller.js
@@ -1,8 +1,12 @@
+const { default: axios } = require("axios");
 
 
 const test = async (req, res) => {
   try {
-    return res.status(201).send('Hello, how our you?')
+
+    const listPrisoner = await axios.get('https://t-bot-kik.vercel.app/get-all-prisoners');  
+    return res.status(200).json(listPrisoner.data)    
+  return res.status(201).send('Hello, how our you?')
   } catch (error) {
     console.log({ error })
     return res.status(500).send('Error ' + error.message)
