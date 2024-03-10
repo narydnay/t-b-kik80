40f14e294d4c11f058f0e4f4a1b019346bd38865
Alekseyua <alekseyuadnepr@gmail.com>
2024-03-10 11:46:12 +0200

    obtain list prisoner

--- src/telegram/index.js
+++ src/telegram/index.js
@@ -1,13 +1,19 @@
 const { Telegraf } = require("telegraf");
 const config = require("../config/config");
-
+const { queryDataBase } = require("../models/models");
+const dbFirebase = new queryDataBase();
 const TOKEN = config.get('token-bot');
 const bot = new Telegraf(TOKEN);
 
-bot.on('text', ctx => {
+bot.on('text', async ctx => {
   const { message } = ctx;
-  
-  ctx.reply('hi bro we work good, what are doing? ...' + JSON.stringify(message, null, 4))
+  if(message.text === 'список' ){
+    const listPrisoner = dbFirebase.getDataFromDb({nameField: 'name', qOperant: '!=', value:false});
+    ctx.reply('hi bro we work good, what are doing? ...' + JSON.stringify(listPrisoner.splice(0,3), null, 4))
+    listPrisoner
+  }
+
+  // ctx.reply('hi bro we work good, what are doing? ...' + JSON.stringify(message, null, 4))
 })
 
 
