7f4742a532092b3a8caf3c73d8ace28befbb2b8e
Alekseyua <alekseyuadnepr@gmail.com>
2024-03-10 13:21:21 +0200

    add axios

--- src/telegram/index.js
+++ src/telegram/index.js
@@ -1,6 +1,7 @@
 const { Telegraf } = require("telegraf");
 const config = require("../config/config");
 const { queryDataBase } = require("../models/models");
+const { default: axios } = require("axios");
 const TOKEN = config.get('token-bot');
 
 const bot = new Telegraf(TOKEN);
@@ -10,7 +11,7 @@ bot.on('text', async ctx => {
   const { message } = ctx;
   if(message.text === 'список' ){
     try{
-      const listPrisoner = await dbFirebase.getDataFromDb({nameField: 'name', qOperant: '!=', value:false});            
+      const listPrisoner = axios.get('http://localhost:5001/api/get-all-prisoners')
       return ctx.reply('hi bro we work good, what are doing? ...' + JSON.stringify(listPrisoner.splice(0,3), null, 4))
     }catch(err){
       return ctx.reply('ERROR? ...\n' + JSON.stringify(err, null, 4))
